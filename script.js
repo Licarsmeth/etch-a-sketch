7a46ab09f1d0a4ae1d75f9be19c0a415c75edb58
Licar <84367661+Licarsmeth@users.noreply.github.com>
2023-08-15 18:59:56 +0545

    Add a function that dynamically sets the grid size

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-const container = document.querySelector('.container');
+const container = document.querySelector(".container");
+const title = document.querySelector(".title");
+const slider = document.querySelector('.slider');
+
 let divCount = 16;
 
+const createGrid = (divCount) => {
+  let newDiv;
+  for (let i = 0; i < divCount * divCount; i++) {
+    newDiv = document.createElement("div");
+    container.appendChild(newDiv);
+  }
+  const allContainer = container.querySelectorAll('div');
+  allContainer.forEach(container => {
+    container.style.flexBasis = `${Math.floor(500/divCount)}px`;
+  })
+};
+
+createGrid(divCount);
