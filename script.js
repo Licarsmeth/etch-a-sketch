0c6dd29bd1e6851183bb479a992f93f8fc026135
Licar <84367661+Licarsmeth@users.noreply.github.com>
2023-08-16 19:54:11 +0545

    Grid gets updated based on the slider value

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 const container = document.querySelector(".container");
 const title = document.querySelector(".title");
-const slider = document.querySelector('.slider');
+const slider = document.querySelector("#slider");
 
-let divCount = 16;
+let divCount = 30;
+
+const removeGrids = (divCount) => {
+  for (let i = 0; i < divCount*divCount; i++) {
+    if (container.firstElementChild) {
+      container.firstElementChild.remove();
+    }
+  }
+};
+
+const addGrids = (divCount) => {
+  createGrid(divCount);
+};
+
+slider.addEventListener("input", () => {
+  removeGrids(divCount);
+  divCount = slider.value;
+  addGrids(divCount);
+});
 
 const createGrid = (divCount) => {
   let newDiv;
@@ -10,10 +28,10 @@ const createGrid = (divCount) => {
     newDiv = document.createElement("div");
     container.appendChild(newDiv);
   }
-  const allContainer = container.querySelectorAll('div');
-  allContainer.forEach(container => {
-    container.style.flexBasis = `${Math.floor(500/divCount)}px`;
-  })
+  const allContainer = container.querySelectorAll("div");
+  allContainer.forEach((container) => {
+    container.style.flexBasis = `${500 / divCount}px`;
+  });
 };
-
 createGrid(divCount);
+
